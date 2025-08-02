597a38c640ace4e33c95e6f562d5f96c6603ff12
NicolasRyj <139501733+NicolasRyj@users.noreply.github.com>
2025-08-02 16:25:12 +0200

    Improved dynamics and structure

---
+++ preSimLoad.m
@@ -0,0 +1,17 @@
+% ===============================
+% Craft Physical & RCS Properties
+% ===============================
+
+craft.mass = 1.0;
+craft.gravity = 9.81;
+craft.inertia = 1.0;
+
+craft.gas.pressure = 1e6;
+craft.gas.temperature = 293.15;
+craft.gas.gamma = 1.3;
+craft.gas.R = 189;
+
+craft.thruster.diameter = 0.001;
+craft.thruster.Cd = 0.85;
+craft.thruster.positionFromCOM = 1.0;
+craft.thruster.gasinitialMass = 0.05;
